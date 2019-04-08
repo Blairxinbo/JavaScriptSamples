9d04a109f1de626cf8282de8105aa31a9cfaed96
xinbo wang <xinbo.wang.dt@gmail.com>
2019-04-07 23:27:45 -0600

    changed

---
+++ ES6/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// require('@babel/register');
+// require('@babel/polyfill');
+// require('./index.js');
+// <script src="https://google.github.io/traceur-compiler/bin/traceur.js"></script>
+// <script src="https://google.github.io/traceur-compiler/bin/BrowserSystem.js"></script>
+// <script src="https://google.github.io/traceur-compiler/src/bootstrap.js"></script>
+// <script type="module">
+//   import './Greeter.js';
+// </script>
+// require('traceur');
+console.log(function (x) {
+  return x * 2;
+}(1));
